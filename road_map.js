80afaaf170169ed3155d456312409b3c6042f44c
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2026-01-29 09:09:24 +0700

    Buổi 1

---
+++ road_map.js
@@ -0,0 +1,33 @@
+const roadMap = {
+    MySQL: {
+        "Buổi 1": "Khải Giảng",
+        "Buổi 2": "MySQL trong docker, Basic CRUD Database, Table",
+        "Buổi 3": "Query, Relationship",
+    },
+    ExpressJS: {
+        "Buổi 4": ["- Khởi tạo source với ExpressJS", "- Áp dụng mô hình MVC"],
+        "Buổi 5": ["- Middleware", "- Áp dụng Middleware"],
+        "Buổi 6": ["- ORM sequilize", "- Database First, Code First", "- ORM prisma", "- Biến Môi Trường"],
+        "Buổi 7": ["- Phân trang, Filter", "- Rap FE", "- CORS"],
+    },
+    Auth: {
+        "Buổi 8": ["- Xây dựng đăng ký đăng nhập", "- AccessToken (json web token)", "- Gửi Email"],
+        "Buổi 9": ["- Sử dụng Middleware bảo vệ API", "- Login Google (BE kiểm soát hết)"],
+        "Buổi 10": ["- Demo hack facebook", "- Refresh Token (json web token)"],
+        "Buổi 11": ["- Upload Hình Local", "- Upload Hình Cloud"],
+        "Buổi 12": ["- Swagger API", "- GraphQL"],
+    },
+    Prisma: {
+        "Buổi 13": ["- Socket IO", "- Xây dựng chức năng chat 1-1 và chat nhóm"],
+        "Buổi 14": ["- Khởi tạo source Nestjs", "- Tìm hiểu cấu trúc của Nestjs"],
+        "Buổi 15": ["- Microservice", "- RabbitMQ"],
+    },
+    NestJS: {
+        "Buổi 16": ["- Cache Memory", "- Cache Redis"],
+        "Buổi 17": ["ElasticSearch", "Kibana"],
+    },
+    Docker: {
+        "Buổi 18": ["- Tìm hiểu về docker", "- Tìm hiểu về docker-compose"],
+        "Buổi 19": ["- Deploy CICD", "- Map Domain"],
+    },
+};
